23fb6d1bd4bc6a5f56715cdf5b59f077ce8e4f02
shayne preston <pandemic2all@gmail.com>
2018-11-21 15:34:42 -0500

    change error msg invalid add user

--- controllers/users.js
+++ controllers/users.js
@@ -24,7 +24,7 @@ module.exports = {
         password: req.body.password,
       })
       .then((users) => res.status(201).send(users))
-      .catch((error) => res.status(400).send(error));
+      .catch((error) => res.status(400).send(400));
   },
 
   getByEmail(req, res) {
