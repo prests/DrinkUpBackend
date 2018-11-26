4da9323d9b1650517b80bf9b6689f87292f21249
shayne preston <pandemic2all@gmail.com>
2018-11-26 10:48:20 -0500

    migrations

--- migrations/20181126154531-fix owner events.js
+++ migrations/20181126154531-fix owner events.js
@@ -2,13 +2,10 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.addColumn("Events", "owner", {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    });
   },
 
   down: (queryInterface, Sequelize) => {
