e17a625a15a5a113be8c27022cf94162f193f2f3
shayne preston <pandemic2all@gmail.com>
2018-11-26 11:30:34 -0500

    migrations

--- migrations/20181126162848-add left attendees bool.js
+++ migrations/20181126162848-add left attendees bool.js
@@ -2,13 +2,9 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.addColumn("Attendees", "left", {
+      type: Sequelize.BOOLEAN
+    })
   },
 
   down: (queryInterface, Sequelize) => {
