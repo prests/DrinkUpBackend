9010f66ee591c6202b8d6973c3ebf1e0481a65c9
shayne preston <pandemic2all@gmail.com>
2018-11-26 10:09:38 -0500

    migrations

--- migrations/20181126020916-remove entered in events.js
+++ migrations/20181126020916-remove entered in events.js
@@ -6,12 +6,9 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.addColumn("Events", "createdAt", {
+      type: Sequelize.DATE,
+      allowNull: false,
+    });
   }
 };
