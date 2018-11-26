08d1006c209ac149bb519120c36e431d7c6207d6
Ubuntu <ubuntu@ip-172-31-40-15.us-east-2.compute.internal>
2018-11-26 15:33:41 +0000

    migrations

--- migrations/20181126153333-rename owners events.js
+++ migrations/20181126153333-rename owners events.js
@@ -2,7 +2,13 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn("Events", "attendancelist");
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
   },
 
   down: (queryInterface, Sequelize) => {
