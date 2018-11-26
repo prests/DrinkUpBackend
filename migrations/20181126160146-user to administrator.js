66786e9cfa10b9229bee0552838ed48ba5d7adf3
shayne preston <pandemic2all@gmail.com>
2018-11-26 11:11:00 -0500

    migrations

--- migrations/20181126160146-user to administrator.js
+++ migrations/20181126160146-user to administrator.js
@@ -2,13 +2,10 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return [
+      queryInterface.renameTable("Users", "Administrators"),
+      queryInterface.renameTable("UsersToEvents", "AdministratorsToEvents")
+    ];
   },
 
   down: (queryInterface, Sequelize) => {
