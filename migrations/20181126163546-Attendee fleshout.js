8aaea8d5bd9319f35b81ab3c97f99f58fcf853c6
shayne preston <pandemic2all@gmail.com>
2018-11-26 11:38:32 -0500

    migrations

--- migrations/20181126163546-Attendee fleshout.js
+++ migrations/20181126163546-Attendee fleshout.js
@@ -2,13 +2,15 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return [
+      queryInterface.addColumn("Attendees", "license_number", {
+        type: Sequelize.INTEGER,
+        allowNulls: false,
+      }),
+      queryInterface.addColumn("Attendees", "middle_name", {
+        type: Sequelize.STRING
+      }),
+    ];
   },
 
   down: (queryInterface, Sequelize) => {
