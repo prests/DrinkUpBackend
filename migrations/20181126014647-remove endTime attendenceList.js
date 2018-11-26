55192287e9ec2f619dcebb01a09c3b0d30e5c253
shayne preston <pandemic2all@gmail.com>
2018-11-25 20:55:54 -0500

    migrations

--- migrations/20181126014647-remove endTime attendenceList.js
+++ migrations/20181126014647-remove endTime attendenceList.js
@@ -6,12 +6,8 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.addColumn("Attendees", "createdAt", {
+      type: Sequelize.DATE
+    })
   }
 };
